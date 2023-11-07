7f5b89610bfaa077f7001b71e0afe0873756e777
LingtianHe <lhe80@ucsc.edu>
2023-11-06 17:07:00 -0800

    manu update

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,10 +18,7 @@ class Menu extends Phaser.Scene {
         this.add.text(game.config.width / 2, game.config.height / 4, "Escape from the Supernova Explosion", title_config).setOrigin(0.5, 0.5)
 
         // game description and controls
-        this.add.text(game.config.width / 2, game.config.height / 2, "You've done evil and created a supernova explosion in a galaxy, and the intergalactic city-states are hunting you as the culprit, dodging a mess of galactic debris and fleeing from your pursuers.", menu_config).setOrigin(0.5, 0.5);
-
-        // credits
-        this.add.text(game.config.width / 2, game.config.height / 1.25, "、Music：", menu_config).setOrigin(0.5, 0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, "You've done evil and created a supernova explosion in a galaxy.", menu_config).setOrigin(0.5, 0.5);
     }
 
     update() {
